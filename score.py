d5b13d2881fdf73af25f9da49b23fc654aa4a54c
Gustavo-Pasciano93 <gustavohqpasciano@gmail.com>
2024-05-04 21:20:22 -0300

    atualizações

--- score.py
+++ score.py
@@ -8,23 +8,35 @@ class Score(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
-        self.goto(0, 280)
+        self.goto(0, 260)
         self.update_score()
         self.hideturtle()
         
         
     def update_score(self):
-        self.write(f"score = {self.score}", align = "center", font=("Arial", 16, "normal"))
+        self.clear()
+        self.write(f"score = {self.score}  High Score: {self.high_score}", align = "center", font=("Arial", 16, "normal"))
+    
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+    
         
-    def game_over(self):
-        self.goto(0, 0) 
-        self.write(f"perdeeeeeu", align = "center", font=("Arial", 16, "normal"))
+    #def game_over(self):
+     #   self.goto(0, 0) 
+      #  self.write(f"perdeeeeeu", align = "center", font=("Arial", 16, "normal"))
         
         
     def increase_score(self):
         self.score += 1
-        self.clear()
+        
         self.update_score()
         
         
--- snake.py
+++ snake.py
@@ -27,6 +27,12 @@ class Snake:
         cobra.goto(position)
         self.segments.append(cobra)
         
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(10000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head =self.segments[0]
     
     
     
